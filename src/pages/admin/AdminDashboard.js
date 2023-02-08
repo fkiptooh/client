f606c592e9e8ce42a6bcf5ac4838d3fe378d7c41
fkiptooh <fkiptooh@gmail.com>
2023-02-08 15:21:14 +0300

    Added a getAllProductByCount function and tested it at the admin dashboard using json stringify method to retrieve all the products from the database

--- src/pages/admin/AdminDashboard.js
+++ src/pages/admin/AdminDashboard.js
@@ -1,14 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import AdminNav from "../../components/nav/AdminNav";
+import { getProductsByCount } from "../../functions/product";
 
 const AdminDashboard =()=>{
+    const [products, setProducts] = useState([]);
+    const [loading, setLoading] = useState(false);
+
+    useEffect(()=>{
+        loadAllProduct();
+    }, []);
+
+    const loadAllProduct =()=> {
+        setLoading(true)
+        getProductsByCount(100)
+        .then(res=> {
+            setProducts(res.data)
+            setLoading(false)
+        })
+        .catch((err)=> {
+            console.log(err)
+            setLoading(false)
+        })
+    }
     return(
         <div className="container-fluid">
         <div className="row">
             <div className="col-md-2">
                 <AdminNav/>
             </div>
-            <div className="col">Admin Dashboard Page</div>
+            {loading ? (<h4 className="text-danger">Loading...</h4>) : (<h4>All Products</h4>)}
+            <div className="col">{JSON.stringify(products)}</div>
         </div>
     </div>
     )
