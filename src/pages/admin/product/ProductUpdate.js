ff238c7f3830a62119e4b9595b2e558381d8b4c4
fkiptooh <fkiptooh@gmail.com>
2023-02-10 00:09:24 +0300

    Consumed the getProduct api based on slug and populate the edit button from the admin panel

--- src/pages/admin/product/ProductUpdate.js
+++ src/pages/admin/product/ProductUpdate.js
@@ -2,20 +2,48 @@ import React,{useState, useEffect} from "react";
 import AdminNav from "../../../components/nav/AdminNav";
 import {toast} from "react-toastify";
 import { useSelector } from "react-redux";
-import {createProduct} from "../../../functions/product"
+import {getProduct} from "../../../functions/product"
 import { getCategories, getSubcategory } from "../../../functions/category";
-import ProductCreateForm from "../../../components/forms/ProductCreateForm";
 import FileUpload from "../../../components/forms/FileUpload";
 import { LoadingOutlined } from "@ant-design/icons";
 import { useParams } from "react-router-dom";
 
+const initialState = {
+    title: '',
+    description:'',
+    price: '',
+    categories: '',
+    category:'',
+    subcategory: [],
+    shipping:'',
+    quantity:'',
+    images: [],
+    colors: ["Black", "Brown", "Silver", "White", "Blue"],
+    brands: ["Apple", "Samsung", "Microsoft", "Lenovo", "ASUS", "HP", "DELL"],
+    color: '',
+    brand: ''
+}
+
 const ProductUpdate =()=> {
+    // state
+    const[values, setValues] = useState(initialState);
 
     let {slug} = useParams()
 
     // redux
     const {user} =  useSelector((state)=>({...state}));
 
+    useEffect(()=>{
+        loadProduct();
+    }, []);
+
+    const loadProduct=()=>{
+        getProduct(slug)
+        .then(p=> {
+            setValues({...values, ...p.data})
+            // console.log(p)
+        });
+    }
     return(
         <div className="container-fluid">
             <div className="row">
@@ -24,7 +52,7 @@ const ProductUpdate =()=> {
                 </div>
                 <div className="col-md-10">
                     <h4>Update Product</h4>
-                    {JSON.stringify(slug)}
+                    {JSON.stringify(values)}
                     <hr/>
                 </div>
             </div>
