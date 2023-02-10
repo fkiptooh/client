d85307909b45a096a058b2f12e2e27a66ad69cb8
fkiptooh <fkiptooh@gmail.com>
2023-02-10 23:47:25 +0300

    Pulled the products from the database to the home page, still working on lots of cool stuff and functionalities

---
+++ src/components/cards/ProductsCard.js
@@ -0,0 +1,32 @@
+import React from "react";
+import laptop from '../../images/laptop.png'
+import { Card } from 'antd'
+import { EyeOutlined, ShoppingCartOutlined } from "@ant-design/icons";
+import { Link } from "react-router-dom";
+
+const {Meta} = Card
+
+const ProductCard =({product})=> {
+    const {title, description, images, slug} = product;
+    return(<div>
+                <Card
+            // eslint-disable-next-line jsx-a11y/alt-text
+            cover={<img src={images && images.length ? images[0].url : laptop}
+                style={{height: '150px', objectFit: "cover"}}
+                className="p-2"
+            /> 
+            }
+            actions={[
+                <Link to={`/product/${slug}`}>
+                    <EyeOutlined className="text-warning"/> <br /> View Product
+                </Link>, 
+                <><ShoppingCartOutlined className="text-danger"/> <br/> Add to Cart</>
+            ]}
+        >
+            <Meta title={title} description={`${description && description.substring(0, 22)}...`}/>
+        </Card>
+    </div>)
+
+}
+
+export default ProductCard;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,40 @@
+import React, {useEffect, useState} from 'react';
+import { getProductsByCount } from '../functions/product'
+import ProductCard from '../components/cards/ProductsCard';
+
 const Home = () => {
+    const [products, setProducts] = useState([]);
+    const [loading, setLoading] = useState(false);
+
+    useEffect(()=>{
+        loadAllProducts();
+    }, []);
+
+    const loadAllProducts =()=> {
+        setLoading(true)
+        getProductsByCount(5)
+        .then(res=>{
+            setProducts(res.data);
+            setLoading(false)
+        })
+    }
     return(
-        <div>
-            <p>React Home</p>
+        <>
+         <div className='jumbotron'>
+            {loading ? <h4>Loading</h4> : <h4>All Products</h4>}
+            {/* {JSON.stringify(products)} */}
         </div>
+        <div className='container'>
+            <div className='row'>
+                {products.map((product)=>(
+                    <div key={product._id} className='col-md-4 pb-3'>
+                        <ProductCard product={product}/>
+                    </div>
+                ))}
+            </div>
+        </div>
+        </>
+       
     );
 };
 
