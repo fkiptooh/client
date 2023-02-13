55397bcf785befc496e84613cef733165308a303
fkiptooh <fkiptooh@gmail.com>
2023-02-13 16:34:18 +0300

    Made image adjustments incase the product image was not created initilly so as to show default product image on the view product

--- src/components/cards/SingleProduct.js
+++ src/components/cards/SingleProduct.js
@@ -4,6 +4,7 @@ import { Link } from "react-router-dom";
 import { HeartOutlined, ShoppingCartOutlined } from "@ant-design/icons";
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from 'react-responsive-carousel';
+import laptop from '../../images/laptop.png'
 
 const { Meta } = Card;
 
@@ -14,9 +15,18 @@ const SingleProduct =({product})=> {
     return(
         <>
             <div className="col-md-7">
-                <Carousel showArrows={true} autoPlay infiniteLoop>
+                {images && images.length ?<Carousel showArrows={true} autoPlay infiniteLoop>
                     {images && images.map((i)=> <img src={i.url} key={i.public_id}/>)}
-                </Carousel>
+                </Carousel> :
+                <Card
+                cover={
+                    <img 
+                        src={laptop}
+                        className="mb-3 card-image"
+                    />
+                }
+                ></Card>
+                }
             </div>
             <div className="col-md-5">
                 <Card
