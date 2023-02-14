4ce1e62fd6e337f0ab42a87220ac5608417a7462
fkiptooh <fkiptooh@gmail.com>
2023-02-14 19:43:39 +0300

    Made changes to the rating modal display only to logged in users

--- src/components/modal/RatingModal.js
+++ src/components/modal/RatingModal.js
@@ -3,15 +3,27 @@ import { Modal, Button } from "antd";
 import { toast } from 'react-toastify';
 import { StarOutlined } from "@ant-design/icons";
 import { useSelector } from 'react-redux';
+import { useNavigate } from "react-router-dom";
 
 
 const RatingModal =({children})=> {
     const { user } = useSelector((state)=> ({...state}));
     const [modalVisible, setModalVisible] = useState(false);
 
+    const navigate = useNavigate();
+
+
+    const handleModal =()=> {
+        if(user && user.token){
+            setModalVisible(true)
+        } else {
+            navigate(`/login`);
+        }
+    }
+
     return(
         <>
-            <div onClick={()=> setModalVisible(true)}>
+            <div onClick={handleModal}>
                 <StarOutlined className="text-danger"/><br/>{" "}
                 {user ? "Leave Rating" : "Login to leave Ratings"}
             </div>
