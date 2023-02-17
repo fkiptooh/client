66780b35d0168e70bc8b72606f38e239113c460a
fkiptooh <fkiptooh@gmail.com>
2023-02-17 09:21:54 +0300

    Added support for displaying categories of products on the home page

---
+++ src/components/category/CategoryList.js
@@ -0,0 +1,31 @@
+import React, {useState, useEffect} from "react";
+import { Link } from "react-router-dom";
+import { getCategories } from '../../functions/category';
+
+const CategoryList=()=> {
+    const [categories, setCategories] = useState([]);
+    const [loading, setLoading] = useState(false);
+
+    useEffect(()=> {
+        setLoading(true)
+        getCategories().then(res=> setCategories(res.data));
+        setLoading(false);  
+    }, []);
+
+    const showCategory =()=>  categories.map((c)=>
+        (<div className="col btn btn-outlined-primary btn-lg btn-block btn-raised m-3">
+            <Link to={`/category/${c.slug}`}>{c.name}</Link>
+        </div>)
+    )
+
+    return(
+        <div className="container">
+            <div className="row">
+                {loading ? <h4 className="text-danger">Loading...</h4> : showCategory()}
+            </div>
+        </div>
+    )
+
+}
+
+export default CategoryList;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Jumbotron from '../components/cards/Jumbotron';
 import NewArrivals from '../components/home/NewArrivals';
 import BestSellers from '../components/home/BestSellers'
+import CategoryList from '../components/category/CategoryList';
 
 const Home = () => {
     return(
@@ -20,6 +21,11 @@ const Home = () => {
             <Jumbotron text={['Best Sellers']} />
         </h4>
         <BestSellers />
+        <br />
+        <h4 className='text-center p-3 mt-5 mb-5 display-4 jumbotron'>
+            <Jumbotron text={['Categories']} />
+        </h4>
+        <CategoryList />
         <br />
 
         </>
