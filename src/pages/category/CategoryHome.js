0559d6d9900f693207f9fa98076c6ff00a95ca0b
fkiptooh <fkiptooh@gmail.com>
2023-02-18 21:56:28 +0300

    Displayed categories on the UI

--- src/pages/category/CategoryHome.js
+++ src/pages/category/CategoryHome.js
@@ -5,20 +5,43 @@ import ProductCard from '../../components/cards/ProductsCard';
 
 const CategoryHome =()=> {
     const [category, setCategory] =  useState({});
-    const [product, setProduct] = useState([]);
+    const [products, setProducts] = useState([]);
     const [loading, setLoading] = useState(false);
 
     const {slug} = useParams();
 
     useEffect(()=> {
         setLoading(true);
-        getCategory(slug).then(c => {
-            console.log(JSON.stringify(c.data, null, 4));
-            setCategory(c.data);
+        getCategory(slug).then(res => {
+            console.log(JSON.stringify(res.data, null, 4));
+            setCategory(res.data.category);
+            setProducts(res.data.products);
+            setLoading(false)
         });
     },[slug]);
 
-    return<p>{slug}</p>
+    return (
+        <div className='container'>
+            <div className="row">
+                <div className="col">
+                    {loading ? (
+                        <h4 className='text-center p-3 mb-5 mt-5 jumbotron'>
+                            Loading
+                        </h4>
+                    ) : (
+                        <h4 className='text-center p-3 mb-5 mt-5 jumbotron'>
+                            {products.length} Products in <b>{category.name}</b> Category
+                        </h4>
+                    )}
+                </div>
+            </div>
+            <div className="row">
+                {products.map((p)=> <div className='col' key={p._id}>
+                    <ProductCard product={p}/>
+                </div>)}
+            </div>
+        </div>
+    )
 
 }
 export default CategoryHome;
